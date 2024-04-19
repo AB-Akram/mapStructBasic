b59f3e9520fc0066597b79f022c7dc10c5f348db
AbdelhakAKRAM <abdelhak.akram@sii-maroc.com>
2024-04-19 11:29:21 +0100

    init commit

---
+++ src/main/java/org/example/mapstructbasic/MapStructBasicApplication.java
@@ -0,0 +1,13 @@
+package org.example.mapstructbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapStructBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapStructBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/mapstructbasic/MapStructBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mapstructbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapStructBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
